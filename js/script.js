2e39d80cefe73a3babb1efe1b9616b788b42c211
Luca <peruzzoluca98@gmail.com>
2021-03-18 12:33:04 +0100

    burger menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
 
 window.onload = () => {
+    this.resizeHeader();
     document.querySelector('.button').addEventListener('click', () => { this.scrollDown() });
 }
 scrollDown = () => {
     const background = document.querySelector('.background');
     window.scrollTo({ behavior: 'smooth', top: background.clientHeight - 50 })
+}
+window.onresize = () => {
+    this.resizeHeader();
+}
+toggleMenu = () => {
+    document.querySelector('.burger-menu').classList.toggle("change");
+    document.querySelector('.menu').classList.toggle('hide');
+}
+resizeHeader = () => {
+    if (window.innerWidth < 1024) {
+        document.querySelector('.mobile').classList.remove('hide');
+        document.querySelector('.desktop').classList.add('hide');
+    } else {
+        document.querySelector('.mobile').classList.add('hide');
+        document.querySelector('.desktop').classList.remove('hide');
+    }
 }
