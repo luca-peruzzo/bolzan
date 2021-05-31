6f9b5767e3791679163792ce04d5b5246123b881
dbolzan <80838100+dbolzan@users.noreply.github.com>
2021-05-31 17:27:26 +0200

    touchstart

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 var modal = document.getElementById("myModal");
 
 // When the user clicks anywhere outside of the modal, close it
+window.ontouchstart = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
